52da7d91e9e2e9fe2257bf58489fc2d19fda8721
Nisha <nisha.kumari@globant.com>
2019-05-30 16:01:46 +0530

    Renamed EMS to ems-service.

--- ems-service/src/main/java/com/globant/EMS/EmsApplication.java
+++ ems-service/src/main/java/com/globant/EMS/EmsApplication.java
@@ -3,12 +3,14 @@ package com.globant.EMS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableOAuth2Sso
 @EnableEncryptableProperties
+@EnableDiscoveryClient
 public class EmsApplication {//implements CommandLineRunner {
 
 	public static void main(String[] args) {
